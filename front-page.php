45c05552b88798f042d85b882a376135653deba4
Surya <hendricks.surya@gmail.com>
2019-10-17 15:11:52 +0200

    ACF to top front-p

--- front-page.php
+++ front-page.php
@@ -9,14 +9,15 @@
 
     <div class="img-g col-lg-6">
         <img class="image-g" src="<?php echo get_field('bandeau_image_g')['sizes']['wpm_taille_1']; ?>">
-        <a href="#" class="survol-g">
+
+
+        
+        <a href="<?php echo $url ?>" <?php echo $target; ?> class="survol-g">
             <h1 class="titre"><?php the_field('bandeau_titre_g') ?></h1>
-            <h2 class="sous-titre">sous titre</h2>
-            <p class="texte-lien">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non debitis delectus dolores
-                perspiciatis
-                necessitatibus sunt inventore, magnam a molestias aut fugiat velit vitae at? Vero qui beatae dolor nam
+            <h2 class="sous-titre"><?php echo get_field('bandeau_survol_g_titre')?></h2>
+            <p class="texte-lien"><?php echo get_field('bandeau_survol_g_texte_g')?>
             </p>
-            <p class="titre-lien">découvrir le magasin <img class="fleche-lien-g" src="<?php bloginfo('template_url'); ?>/assets/img/svg/Group 1300.svg" alt="flèche">
+            <p class="titre-lien"><?php echo get_field('bandeau_btn_g_title')?><img class="fleche-lien-g" src="<?php bloginfo('template_url'); ?>/assets/img/svg/Group 1300.svg" alt="flèche">
             </p>
             <div class="bloc-fleche-g">
                 <img class="fleche-g" src="<?php bloginfo('template_url'); ?>/assets/img/svg/fleche-droite.svg" alt="">
@@ -30,14 +31,12 @@
     </div>
     <div class="img-d col-lg-6">
         <img class="image-d" src="<?php echo get_field('bandeau_image_d')['sizes']['wpm_taille_1']; ?>">
-        <a href="#" class="survol-d">
+        <a href="<?php echo $url ?>" <?php echo $target; ?> class="survol-d">
             <h1 class="titre"><?php the_field('bandeau_titre_d') ?></h1>
-            <h2 class="sous-titre">sous titre</h2>
-            <p class="texte-lien">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non debitis delectus dolores
-                perspiciatis
-                necessitatibus sunt inventore, magnam a molestias aut fugiat velit vitae at? Vero qui beatae dolor nam
+            <h2 class="sous-titre"><?php echo get_field('bandeau_survol_d_titre')?></h2>
+            <p class="texte-lien"><?php echo get_field('bandeau_survol_d_texte_d')?>
             </p>
-            <p class="titre-lien">découvrir le magasin <img class="fleche-lien-d" src="<?php bloginfo('template_url'); ?>/assets/img/svg/Group 1300.svg" alt="flèche">
+            <p class="titre-lien"><?php echo get_field('bandeau_btn_d_title')?><img class="fleche-lien-d" src="<?php bloginfo('template_url'); ?>/assets/img/svg/Group 1300.svg" alt="flèche">
             </p>
             <div class="bloc-fleche-d">
                 <img class="fleche-d" src="<?php bloginfo('template_url'); ?>/assets/img/svg/fleche-droite.svg" alt="">
@@ -55,7 +54,7 @@
 
 
 
-<h2 class = "groupe"><?php echo get_field('bandeau_survol_g_titre')?></h2>
+
 
 
 
