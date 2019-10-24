48a22cc737cecc931cdcb67e35793e2828b58c84
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-10-24 09:33:32 +0200

    merged from yousef-dev brancg

--- footer.php
+++ footer.php
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-<footer>
-    <div class = "footer container">
-        <div class = "map col-lg-4">
-             <h5>Rendez-nous visite !</h5>
-             <a class="google-map" href="#">Google map <img src="<?php bloginfo('template_url'); ?>/assets/img/svg/Group 1300.svg" alt=""></a>
-        </div>
-        <div class = "adress col-lg-4">
-            <p><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/marker-road.svg" alt="">Route de Hamoir, 87 – 4500 Huy </p>
-           <p><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/icon-arrobas.svg" alt=""><a href="#">info@meubles-composition.be</a></p>
-           <p><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/phone-2.svg" alt=""><a href="">+32 (0)85 21 52 38</a>  </p>
-        </div>
-      <div class = "sm-icons col-lg-4">
-          <div class="justIcons">
-=======
 <?php wp_footer(); ?>
 <div class="footer container">
     <div class="map col-lg-4">
@@ -27,7 +11,6 @@
     </div>
     <div class="sm-icons col-lg-4">
         <div class="justIcons">
->>>>>>> yousef-dev
             <span><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/logo-facebook.svg" alt=""> </a> </span>
             <span><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/logo-instagram.svg" alt=""> </a> </span>
             <span><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/logo-pinterest.svg" alt=""> </a> </span>
--- functions.php
+++ functions.php
@@ -1,14 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 
-add_image_size('wpm_taille_1', 925, 600);
-=======
-  
-add_image_size( 'wpm_taille_1', 1500, 1250); 
-add_image_size( 'wpm_taille_2', 1000, 1600);
-add_image_size( 'wpm_taille_3', 1800, 1200);
->>>>>>> 730c4ebdca2faf82293755119a0efd2b95f542fe
+add_image_size('wpm_taille_1', 1500, 1250);
+add_image_size('wpm_taille_2', 1000, 1600);
+add_image_size('wpm_taille_3', 1800, 1200);
 
 // enlever l'éditeur de texte Gutenberg
 // for posts
@@ -26,28 +21,20 @@ add_theme_support('title-tag');
 
 
 //add CSS to News page
-function add_news_styles() {
-  wp_enqueue_style( 'news-style', get_template_directory_uri() . '/assets/CSS/news-style.css' );
-  };
-  add_action( 'wp_enqueue_scripts', 'add_news_styles' );
-//
-
-<<<<<<< HEAD
-function prefix_add_archi_styles()
+function add_news_styles()
 {
-  wp_enqueue_style('archi-dinterieur-style', get_template_directory_uri() . '/assets/CSS/archi-dinterieur-style.css');
+  wp_enqueue_style('news-style', get_template_directory_uri() . '/assets/CSS/news-style.css');
 };
-add_action('wp_enqueue_scripts', 'prefix_add_archi_styles');
-
-//contact style here ...
-=======
-//add CSS to Single page
-function add_single_styles() {
-  wp_enqueue_style( 'single-style', get_template_directory_uri() . '/assets/CSS/single-style.css' );
-  };
-  add_action( 'wp_enqueue_scripts', 'add_single_styles' );
+add_action('wp_enqueue_scripts', 'add_news_styles');
 //
->>>>>>> 730c4ebdca2faf82293755119a0efd2b95f542fe
+
+//add CSS to Single page
+function add_single_styles()
+{
+  wp_enqueue_style('single-style', get_template_directory_uri() . '/assets/CSS/single-style.css');
+};
+add_action('wp_enqueue_scripts', 'add_single_styles');
+//
 
 function prefix_add_contact_styles()
 {
@@ -60,33 +47,19 @@ add_action('wp_enqueue_scripts', 'prefix_add_contact_styles');
 //end contact style .....
 
 //add CSS to Footer
-<<<<<<< HEAD
 function prefix_add_footer_styles()
 {
-  wp_enqueue_style('your-style-id', get_template_directory_uri() . '/assets/CSS/style.css');
+  wp_enqueue_style('your-style-id', get_template_directory_uri() . '/assets/CSS/style.css', array(), '1.0', 'all');
 };
 add_action('get_footer', 'prefix_add_footer_styles');
 
 //
 function wpa_90820()
 {
-  wp_enqueue_style('my-styles', get_stylesheet_directory_uri() . '/assets/CSS/style.css', array(), '1.0');
+  wp_enqueue_style('my-styles', get_stylesheet_directory_uri() . '/assets/CSS/style.css', array(), '1.1');
 }
 
 add_action('wp_enqueue_scripts', 'wpa_90820');
-=======
-function prefix_add_footer_styles() {
-  wp_enqueue_style( 'your-style-id', get_template_directory_uri() . '/assets/CSS/style.css', array(), '1.0', 'all' );
-  };
-  add_action( 'get_footer', 'prefix_add_footer_styles' );
-
-  //
-function wpa_90820() {
-  wp_enqueue_style('my-styles', get_stylesheet_directory_uri() .'/assets/CSS/style.css', array(), '1.1' );
-  }
-  
-  add_action('wp_enqueue_scripts', 'wpa_90820');
->>>>>>> 730c4ebdca2faf82293755119a0efd2b95f542fe
 
 
 //Ajouter custome menu
